ee957921c6d56c31d441426897e1a0e9c6aa80b0
AlihBangunWicaksono <alihbangun008@gmail.com>
2026-02-23 15:57:23 +0700

    fix database.js

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ console.log(`📡 Connecting to DB at ${process.env.DB_HOST}:${process.env.DB_PO
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
